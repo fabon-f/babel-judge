96ce8eb3e83bdd044ff63d570cea9bd22167ec63
fabon <syobon.hinata.public@gmail.com>
2023-05-20 18:44:25 +0900

    Remove debug log

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,11 +1,5 @@
 import '@hotwired/turbo-rails';
 
-console.log('Vite ⚡️ Rails');
-console.log(
-  'Visit the guide for more information: ',
-  'https://vite-ruby.netlify.app/guide/rails'
-);
-
 // Example: Load Rails libraries in Vite.
 //
 // import * as Turbo from '@hotwired/turbo'
