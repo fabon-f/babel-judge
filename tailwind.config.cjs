a264e0d88820a20e72c27175581ec6c3e55e77ae
fabon <syobon.hinata.public@gmail.com>
2023-05-20 12:04:56 +0900

    Use Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*',
+    './app/frontend/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import lightningcss from 'vite-plugin-lightningcss'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    lightningcss({
+      browserslist: '>= 0.25%',
+    }),
   ],
 })
